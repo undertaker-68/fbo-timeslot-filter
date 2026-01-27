61dc3b42252741bf126c1312b44d6651a00fa704
undertaker-68 <k10039872@gmail.com>
2026-01-27 11:37:47 +0700

    Update fbo_supply_list_cli.py

--- src/fbo_supply_list_cli.py
+++ src/fbo_supply_list_cli.py
@@ -117,7 +117,7 @@ def main():
             try:
                 ms_result = sync_orders_to_ms(client, client.name.upper(), kept)
                 logger.info("[%s] MS DRY result: %s", client.name, ms_result)
-                ms_moves_result = sync_moves_from_orders(client, client.name.upper(), kept)
+                #ms_moves_result = sync_moves_from_orders(client, client.name.upper(), kept)
                 logger.info("[%s] MS MOVES result: %s", client.name, ms_moves_result)
                 if (os.getenv("SYNC_DEMANDS") or "").strip() == "1":
                     ms_demands_result = sync_demands_from_orders(client, client.name.upper(), kept)
